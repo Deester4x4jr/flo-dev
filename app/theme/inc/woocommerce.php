88b7c2d12d7a599e0e30565563afa88da91ab3fc
Josh DeSherlia <josh@desherlia.net>
2017-03-08 18:25:55 -0700

    working on submodules

--- app/theme/inc/woocommerce.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$files = glob( get_theme_file_uri( '/inc/woocommerce/*.php' ) );
-foreach ( $files as $file ) {
-  require_once( $file );
-}
--- app/theme/js/src/01.includes.js
+++
@@ -1,7 +0,0 @@
-//=require es6-promise.auto.min.js
-//=require sweetalert2.min.js
-//=require ellipsis.min.js
-//=require ScrollMagic.min.js
-//=require animation.gsap.min.js
-//=require js.cookie.js
-//=require responsive-nav.min.js
